12cab35664c12494032e100662789e4fb9b34c3f
Aleksey Lisun <lisun90@gmail.com>
2013-12-31 16:46:13 +0200

    Add local inner class

--- src/MyOuter.java
+++ src/MyOuter.java
@@ -5,49 +5,53 @@
  * Time: 4:04 PM
  */
 public class MyOuter {
+
     private int x = 7;
 
-    MyInner inner = new MyInner();
-
-    public void seeInner() {
+    public void makeInner() {
+        MyInner inner = new MyInner();
         inner.seeOuter();
     }
 
     private class MyInner {
-        private int i;
-        private String string;
-
-
         public void seeOuter() {
-            System.out.println(x);
-        }
-
-        public int getI() {
-            return i;
-        }
-
-        public void setI(int i) {
-            this.i = i;
-        }
-
-        public String getString() {
-            return string;
-        }
-
-        public void setString(String string) {
-            this.string = string;
+            System.out.println("Outer x is: " + x);
+            System.out.println("Inner class ref is " + this);
+            System.out.println("Outer class ref is " + MyOuter.this);
         }
     }
 
-    public static void main(String[] args) {
+    public void localInnerClass(final int i) {
+        final String string = "LocalInner: ";
 
+        class LocalInner {
+            private void seeLocalParameter() {
+                System.out.println(string + x);
+            }
+        }
+        final LocalInner localInner = new LocalInner();
+        localInner.seeLocalParameter();
+    }
+
+
+    private static class StaticNestedClass {
+
+
+    }
+
+
+    public static void main(String[] args) {
+        // Regular inner instantiation and methods call
         final MyOuter outer = new MyOuter();
-        outer.seeInner();
+        outer.makeInner();
         MyOuter.MyInner myInner = outer.new MyInner();
         myInner.seeOuter();
 
         MyOuter.MyInner myInner1 = new MyOuter().new MyInner();
         myInner1.seeOuter();
+        //Call local inner class
+
+        outer.localInnerClass(6);
 
 
     }
