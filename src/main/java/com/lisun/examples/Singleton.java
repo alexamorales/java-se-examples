62b623da53003a7ff02b386835bc765bf61953f2
Aleksey Lisun <lisun90@gmail.com>
2014-01-02 22:47:48 +0200

    Test folders added Singleton example (lazy init) added

---
+++ src/main/java/com/lisun/examples/Singleton.java
@@ -0,0 +1,34 @@
+package com.lisun.examples;
+
+/**
+ * @author Aleksey
+ * @since 31.12.13
+ */
+public class Singleton {
+    private Singleton() {
+
+    }
+
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+
+        Singleton singleton1 = Singleton.getInstance();
+
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton1 == singleton2);
+
+    }
+
+    //TODO add another variant of Singleton
+
+
+}
