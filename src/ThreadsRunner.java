46a638adadc6f0ce42ea9a84a3d512250a672d73
Aleksey Lisun <oleksii.lisun@infopulse.com.ua>
2014-01-02 17:33:47 +0200

    Multithreading examples added

---
+++ src/ThreadsRunner.java
@@ -0,0 +1,64 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Oleksii.Lisun
+ * Date: 10/23/13
+ * Time: 4:11 PM
+ */
+class ThreadRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " " + this.getClass().getName() + "i: " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class TestThread extends Thread  {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+            System.out.println(Thread.currentThread().getName() + " " + this.getClass().getName() + "i: " + i);
+        }
+    }
+}
+
+
+public class ThreadsRunner {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread1 = new Thread(new ThreadRunnable());
+        Thread thread2 = new Thread(new ThreadRunnable());
+
+        TestThread thread3 = new TestThread();
+        TestThread thread4 = new TestThread();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "i: " + i);
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
